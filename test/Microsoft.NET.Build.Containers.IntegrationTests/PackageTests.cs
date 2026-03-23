fd8270e1f264a0b4b8fe722ad7cad91d24741c95
Yuliia Kovalova <ykovalova@microsoft.com>
2026-03-23 18:09:20 +0100

    more cleanup

--- test/Microsoft.NET.Build.Containers.IntegrationTests/PackageTests.cs
+++ test/Microsoft.NET.Build.Containers.IntegrationTests/PackageTests.cs
@@ -42,7 +42,6 @@ public class PackageTests
             "Microsoft.Build.Utilities.Core",
             "Microsoft.CodeAnalysis.PublicApiAnalyzers",
             "Nuget.Packaging",
-            "System.Text.Json",
             "Valleysoft.DockerCredsProvider",
             "Microsoft.Extensions.Logging",
             "Microsoft.Extensions.Logging.Abstractions"
